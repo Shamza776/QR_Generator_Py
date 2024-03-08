a9cc0e9b8516df4ab7440e24d40fbcebac3a7b2a
Shamza <sharleenjamoza@gmail.com>
2024-03-08 12:11:56 +0300

    self password generator

---
+++ passwordGenerator1.py
@@ -0,0 +1,7 @@
+import random
+
+characters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+#upperCharacter = characters.upper()
+#print(upperCharacter)
+
+
